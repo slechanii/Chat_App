8e870afbe099ee40398f167f9804ab18432df662
Sofian Lechani <slechani@gmail.com>
2020-12-21 18:48:41 +0100

    chatbox save message content on type + call function on button press

--- frontend/src/Components/Chatbox.js
+++ frontend/src/Components/Chatbox.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react'
-import { Grid, Input, GridRow, Container, TextArea } from 'semantic-ui-react'
+import { Grid, Input, GridRow, Container, TextArea, Button, ButtonGroup, Icon } from 'semantic-ui-react'
 
 export default class Chatbox extends Component {
+
+    state = {
+        messageContent : "",       
+    }
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+    sendMessage = () => {
+        alert(this.state.messageContent)
+    }
+
     render() {
         return (
             <Grid.Row className="chatbox-container">
                 <Grid className="chatbox">
-                    <Grid.Row className="chatbox-textarea-row">  
-                <TextArea placeholder="Send a message to #channel" className="chatbox-input"></TextArea>
+                    <Grid.Row className="chatbox-textarea-row">
+                        <TextArea name="messageContent" onChange={this.handleChange} placeholder="Send a message to #channel" className="chatbox-input"></TextArea>
                     </Grid.Row>
                     <GridRow className="chatbox-formatting-row">
-                    <button></button>
-                     </GridRow>
+                        <Button.Group>
+                            <Button icon>
+                                <Icon name='bold' />
+                            </Button>
+                            <Button icon>
+                                <Icon name='italic' />
+                            </Button>
+                            <Button icon>
+                                <Icon name='strikethrough' />
+                            </Button>
+                            <Button icon>
+                                <Icon name='code' />
+                            </Button>
+                            <Button icon>
+                                <Icon name='ordered list' />
+                            </Button>
+                            <Button icon>
+                                <Icon name='bulleted list' />
+                            </Button>
+                            <Button icon>
+                                <Icon name='quote left' />
+                            </Button>
+                        </Button.Group>
+                    
+                            <Button onClick={this.sendMessage} icon>
+                                <Icon name='send' />
+                            </Button>
+                       
+
+
+                    </GridRow>
                 </Grid>
             </Grid.Row>
 
