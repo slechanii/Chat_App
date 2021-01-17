b9701bb9265785fdf47cd072f875e80cab132a2d
Sofian Lechani <slechani@gmail.com>
2021-01-17 08:56:17 +0100

    dynamic width for date container depending on date length

--- frontend/src/Components/DateSeparator.js
+++ frontend/src/Components/DateSeparator.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import { Divider } from 'semantic-ui-react';
+import classNames from "classnames"
 
 export default class DateSeparator extends Component {
     render() {
+        let wrapper_classes = classNames(
+            'date-separator-wrapper',
+            {
+                'yesterday-date': this.props.date === "Yesterday",
+                'today-date': this.props.date === "Today",
+                'other-date': this.props.date != "Yesterday" && this.props.date != "Today",
+            }
+        );
         return (
             <div className="date-separator-container">
          
                     <div className="divider-date-separator"></div>
-                    <div className="date-separator-wrapper">
+                    <div className={wrapper_classes}>
                         <span className="date-separator-date">{this.props.date}</span>
                     </div>
                 
